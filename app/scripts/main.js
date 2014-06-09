b2ee48efe4f8b3e450258c40a0188ccc86d6a543
adam bezemek <adam.bezemek@gmail.com>
2014-06-09 09:13:20 -0500

    everything is awesome... everything is cool when your working with me. styling complete for desktop, mobile needs some tweaking still

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,40 +1,16 @@
-jQuery(function( $ ){
-
-    /* --- Smooth scroll effect --- */
-
-    $.localScroll({
-        duration: 750
-    });
-
-    /* --- More scrolling effects --- */
-
-    scrolltop = $(window).scrollTop();
-    scrollwindow = scrolltop + $(window).height();
-
-    if( scrollwindow < ( $(".feature").offset().top + 200 ) )
-        $(".feature").css("opacity", "0");
-
-    // Enable parallax and fade effects
-    $(window).scroll(function(){
-
-        scrolltop = $(window).scrollTop();
-        scrollwindow = scrolltop + $(window).height();
-        introheight = $("#jumbo").height();
-
-        // $(".testimonial-wrap").css("backgroundPosition", "center " + (scrolltop/4) + "px");
-
-//        $(".jumbotron-content").css("opacity", 1 - (scrolltop/500));
-//        $(".jumbotron-content h1").css("top", 1 - (scrolltop/10) + "px");
-
-        if( scrollwindow > ( $(".features").offset().top + 200 ) ) {
-
-            $(".features .feature-one").animate({ opacity: 1 }, 1000);
-            $(".features .feature-two").delay(250).animate({ opacity: 1 }, 1000);
-            $(".features .feature-three").delay(500).animate({ opacity: 1 }, 1000);
-            $(".features .feature-four").delay(750).animate({ opacity: 1 }, 1000);
-
-        }
-
-    });
-
+$('.main').onepage_scroll({
+    sectionContainer: 'section',     // sectionContainer accepts any kind of selector in case you don't want to use section
+    easing: 'ease',                  // Easing options accepts the CSS3 easing animation such 'ease', 'linear', 'ease-in',
+                                     // 'ease-out', 'ease-in-out', or even cubic bezier value such as 'cubic-bezier(0.175, 0.885, 0.420, 1.310)'
+    animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+    pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+    updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+    beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+    afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+    loop: true,                      // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+    keyboard: true,                  // You can activate the keyboard controls
+    responsiveFallback: 768,       // You can fallback to normal page scroll by defining the width of the browser in which
+                                     // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                                     // the browser's width is less than 600, the fallback will kick in.
+    direction: 'vertical'            // You can now define the direction of the One Page Scroll animation. Options available are 'vertical' and 'horizontal'. The default value is 'vertical'.
 });
